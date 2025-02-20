56c4e864112d347c303481d13980ba3af63fe1ba
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2025-02-20 16:08:25 -0500

    add percept config

--- mp_eval/classes/workload.py
+++ mp_eval/classes/workload.py
@@ -34,11 +34,60 @@ class PlannerConfig:
     max_prediction_steps: int
     poses: Poses # to be stored in start_goal.yaml
     agents: List[AgentConfig] # to be stored in scenario_config.yaml
+
+@dataclass
+class SceneParams:
+    static_scene_path: str
+    scene_generation_type: str
+    scene_generation_params: Dict[str, float]
+
+    @classmethod
+    def from_config(cls, config: Dict):
+        return cls(
+            static_scene_path=config['static_scene_path'] if 'static_scene_path' in config else None,
+            scene_generation_type=config['scene_generation_type'] if 'scene_generation_type' in config else None,
+            scene_generation_params=config['scene_generation_params'] if 'scene_generation_params' in config else None
+        )
+@dataclass
+class SceneConfig:
+    scene_type: str
+    scene_params: SceneParams
+    @classmethod
+    def from_config(cls, config: Dict):
+        return cls(
+            scene_type=config['scene_type'],
+            scene_params=SceneParams.from_config(config['scene_params'])
+        )
+@dataclass
+class FieldsConfig:
+    k_circular_force: float
+    agent_radius: float
+    mass_radius: float
+    max_allowable_force: float
+    detect_shell_radius: float
+    publish_force_vector: bool
+    show_processing_delay: bool
+@dataclass
+class PerceptConfig:
+    namespace: str
+    mode: str
+    scene_config: SceneConfig
+    fields_config: FieldsConfig
+
+    @classmethod
+    def from_config(cls, config: Dict, planner_config: PlannerConfig):
+        return cls(
+            namespace=planner_config.experiment_type,
+            mode=config['mode'],
+            scene_config=SceneConfig.from_config(config['scene_config']),
+            fields_config=FieldsConfig(**config['fields_config'])
+        )
+
 @dataclass
 class WorkloadConfig:
     metadata: Metadata
     planner_config: PlannerConfig
-    # scene_config: SceneConfig
+    percept_config: PerceptConfig
 
     @classmethod
     def from_yaml(cls, yaml_path: str):
@@ -65,8 +114,12 @@ class WorkloadConfig:
                 agents=agents
             )
             
+            percept_data = data['percept_config']
+            percept_config = PerceptConfig.from_config(percept_data, planner_config)
+
             return cls(
                 metadata=metadata,
-                planner_config=planner_config
+                planner_config=planner_config, 
+                percept_config=percept_config
             )
 
