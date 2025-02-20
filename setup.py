8a9a238d604ceb690d7287c6c7c573cda2701891
Tanmay Bishnoi <tbishnoi@torontomu.ca>
2025-02-19 23:26:51 -0500

    init package

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'mp_eval'
 
 setup(
     name=package_name,
     version='0.0.0',
-    # packages=find_packages(exclude=['test']),
-    # data_files=[
-    #     ('share/ament_index/resource_index/packages',
-    #         ['resource/' + package_name]),
-    #     ('share/' + package_name, ['package.xml']),
-    # ],
-    # install_requires=['setuptools'],
-    # zip_safe=True,
-    # maintainer='dev',
-    # maintainer_email='tbishnoi@torontomu.ca',
-    # description='TODO: Package description',
-    # license='TODO: License declaration',
-    # tests_require=['pytest'],
-    # entry_points={
-    #     'console_scripts': [
-    #     ],
-    # },
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.py'))),
+        (os.path.join('share', package_name, 'assets'), glob(os.path.join('assets', '*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='dev',
+    maintainer_email='tbishnoi@torontomu.ca',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'mp_eval = mp_eval.mp_eval:main',
+        ],
+    },
 )
